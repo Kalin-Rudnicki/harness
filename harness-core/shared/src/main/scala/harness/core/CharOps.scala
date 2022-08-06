a56190a217136e9bb8cb884d47912f6ee90d9c47
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2022-08-06 13:26:14 -0600

    HARNESS-21 : Add Char extensions

---
+++ harness-core/shared/src/main/scala/harness/core/CharOps.scala
@@ -0,0 +1,19 @@
+package harness.core
+
+extension (self: Char) {
+
+  def unesc: String = self.unesc("'")
+  def unesc(leftAndRight: String): String = self.unesc(leftAndRight, leftAndRight)
+  def unesc(left: String, right: String): String = {
+    val charText =
+      self match {
+        case '\n' => "\\n"
+        case '\\' => "\\\\"
+        case '\t' => "\\t"
+        case '"'  => "\\\""
+        case c    => c.toString
+      }
+    s"$left$charText$right"
+  }
+
+}
