b026b36f6f199739fe9181df1a2b22d93ba03121
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2022-09-28 20:57:33 -0600

    query-set-const

--- harness-sql/src/main/scala/harness/sql/query/QuerySet.scala
+++ harness-sql/src/main/scala/harness/sql/query/QuerySet.scala
@@ -34,6 +34,19 @@ object QuerySetOps {
         QueryInputMapper(_ => 1, (in, out, off) => out(off) = a.col.colCodec.encoder.encodeColumn(in(b.idx).some.asInstanceOf)),
       )
 
+  implicit def col_const[A]: QuerySetOps[AppliedCol[A], Constant[A]] =
+    (a, b) =>
+      QuerySet(
+        s"${a.ref.colName} = ?",
+        QueryInputMapper(_ => 1, (_, out, off) => out(off) = a.col.colCodec.encoder.encodeColumn(b.value)),
+      )
+  implicit def oCol_const[A]: QuerySetOps[AppliedCol[Option[A]], Constant[A]] =
+    (a, b) =>
+      QuerySet(
+        s"${a.ref.colName} = ?",
+        QueryInputMapper(_ => 1, (_, out, off) => out(off) = a.col.colCodec.encoder.encodeColumn(b.value.some)),
+      )
+
 }
 
 extension [A](a: A) {
