1baa6ccac6e7b61ffe0323cab0a8cb10734a5f57
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2022-12-31 11:28:38 -0700

    String case conversions

--- harness-core/shared/src/main/scala/harness/core/StringOps.scala
+++ harness-core/shared/src/main/scala/harness/core/StringOps.scala
@@ -1,6 +1,10 @@
 package harness.core
 
 import cats.syntax.option.*
+import scala.util.matching.Regex
+
+private val camelRegex: Regex = "\\d+|[A-Z](?=[a-z])|[A-Z]+(?:(?=$)|(?![^A-Z]))".r
+private val snakeRegex: Regex = "_[a-zA-Z0-9]".r
 
 extension (self: String) {
 
@@ -11,6 +15,10 @@ extension (self: String) {
 
   def pluralize(amount: Long, pluralSuffix: String = "s", singularSuffix: String = ""): String =
     s"$self${if (amount.abs == 1) singularSuffix else pluralSuffix}"
+
+  def decapitalize: String =
+    if (self == null || self.length == 0 || !self.charAt(0).isUpper) self
+    else self.updated(0, self.charAt(0).toLower)
 
   // =====| Alignment |=====
 
@@ -40,5 +48,22 @@ extension (self: String) {
 
   def stripColor: String =
     self.replaceAll("\u001b\\[\\d+(;\\d+)*m", "")
+
+  // =====| Casing |=====
+
+  def camelToSnake: String =
+    camelRegex.replaceAllIn(self, m => if (m.start == 0) m.matched else s"_${m.matched}").toLowerCase
+
+  def snakeToLowerCamel: String =
+    snakeRegex.replaceAllIn(self, _.matched.substring(1).capitalize)
+
+  def snakeToUpperCamel: String =
+    self.snakeToLowerCamel.capitalize
+
+  def dashToSnake: String =
+    self.replace('-', '_')
+
+  def snakeToDash: String =
+    self.replace('_', '-')
 
 }
