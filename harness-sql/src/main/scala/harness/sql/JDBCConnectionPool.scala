3fdc520f85fd7e5d42240e36a6faf9829487b0d4
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2023-01-07 19:11:11 -0700

    tmp-connection-pool

--- harness-sql/src/main/scala/harness/sql/JDBCConnectionPool.scala
+++ harness-sql/src/main/scala/harness/sql/JDBCConnectionPool.scala
@@ -7,10 +7,14 @@ import zio.*
 final class JDBCConnectionPool private (val pool: ZPool[HError, JDBCConnection])
 object JDBCConnectionPool {
 
-  def apply(cf: ConnectionFactory, min: Int, max: Int, duration: Duration): HRIO[Scope, JDBCConnectionPool] =
-    ZPool.make(cf.getJDBCConnection, min to max, duration).map(new JDBCConnectionPool(_))
+  def apply(cf: ConnectionFactory, min: Int, max: Int, duration: Duration): HRIO[Logger & Scope, JDBCConnectionPool] =
+    Logger.log.debug(s"Creating ConnectionPool($min, $max)") *>
+      ZPool
+        .make(cf.getJDBCConnection, min to max, duration)
+        .map(new JDBCConnectionPool(_))
+        .withFinalizerExit { (_, _) => Logger.log.debug("Releasing ConnectionPool") }
 
-  def layer(min: Int, max: Int, duration: Duration): HRLayer[ConnectionFactory & Scope, JDBCConnectionPool] =
+  def layer(min: Int, max: Int, duration: Duration): HRLayer[ConnectionFactory & Logger & Scope, JDBCConnectionPool] =
     ZLayer.fromZIO { ZIO.service[ConnectionFactory].flatMap(JDBCConnectionPool(_, min, max, duration)) }
 
 }
