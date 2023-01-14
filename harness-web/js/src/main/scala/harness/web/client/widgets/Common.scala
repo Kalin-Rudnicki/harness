1d5926b9cedcc9b35ec5d1f38ea797046063dc99
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2023-01-14 07:44:56 -0700

    Added chunkWidget

--- harness-web/js/src/main/scala/harness/web/client/widgets/Common.scala
+++ harness-web/js/src/main/scala/harness/web/client/widgets/Common.scala
@@ -4,19 +4,32 @@ import cats.syntax.traverse.*
 import harness.web.client.*
 import harness.web.client.vdom.{given, *}
 import monocle.*
+import zio.Chunk
 
 object Common {
 
-  private def lensForIndex[S](idx: Int): Lens[List[S], S] =
+  private def listIndexLens[S](idx: Int): Lens[List[S], S] =
     Lens[List[S], S](_(idx))(elem => list => list.updated(idx, elem))
+
+  private def chunkIndexLens[S](idx: Int): Lens[Chunk[S], S] =
+    Lens[Chunk[S], S](_(idx))(elem => list => list.updated(idx, elem))
 
   def listWidget[Action, State, Value](widget: ModifierAV[Action, State, Value]): ModifierAV[Action, List[State], List[Value]] =
     PModifier.Simple[Action, List[State], List[State], List[Value]](
       (rh, listS) =>
         listS.indices.toList.flatMap { idx =>
-          widget.imapState(lensForIndex(idx)).build(rh, listS)
+          widget.imapState(listIndexLens(idx)).build(rh, listS)
         },
       _.traverse(widget.value),
+    )
+
+  def chunkWidget[Action, State, Value](widget: ModifierAV[Action, State, Value]): ModifierAV[Action, Chunk[State], Chunk[Value]] =
+    PModifier.Simple[Action, Chunk[State], Chunk[State], Chunk[Value]](
+      (rh, listS) =>
+        listS.indices.toList.flatMap { idx =>
+          widget.imapState(chunkIndexLens(idx)).build(rh, listS)
+        },
+      _.toList.traverse(widget.value).map(Chunk.fromIterable),
     )
 
 }
