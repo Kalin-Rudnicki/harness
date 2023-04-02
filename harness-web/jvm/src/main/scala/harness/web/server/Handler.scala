2003ab88a15b9a30f50abab642f1a4ba93b8f731
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2023-04-02 15:40:05 -0600

    Only log HttpRequest Handler once

--- harness-web/jvm/src/main/scala/harness/web/server/Handler.scala
+++ harness-web/jvm/src/main/scala/harness/web/server/Handler.scala
@@ -25,52 +25,51 @@ final case class Handler[ServerEnv, ReqEnv: EnvironmentTag](
         Scope.default
 
     val effect: SHRIO[ServerEnv & BuiltInRequestEnv & ReqEnv, Unit] =
-      ZIO.serviceWithZIO[HttpRequest] { req =>
-        (for {
-          now <- Clock.currentDateTime
-          responseBody <- ZIO.fromAutoCloseable { ZIO.succeed(exchange.getResponseBody) }
-          ifHidden <- ZIO.service[HError.UserMessage.IfHidden]
+      for {
+        req <- HttpRequest.service
+        now <- Clock.currentDateTime
+        responseBody <- ZIO.fromAutoCloseable { ZIO.succeed(exchange.getResponseBody) }
+        ifHidden <- ZIO.service[HError.UserMessage.IfHidden]
 
-          _ <- Logger.log.info(s"received ${req.method.method} request @ '${req.pathString}'", "remote-address" -> req.remoteAddress, "date" -> now.toLocalDate, "time" -> now.toOffsetTime)
+        _ <- Logger.log.info(s"received ${req.method.method} request @ '${req.pathString}'", "remote-address" -> req.remoteAddress, "date" -> now.toLocalDate, "time" -> now.toOffsetTime)
 
-          responseOrError <- route(req.method, req.path).either
-          responseOrError <- responseOrError match {
-            case Left(earlyReturn: EarlyReturn) => Logger.log.debug("Received early return value").as(earlyReturn.response.asRight)
-            case other                          => ZIO.succeed(other)
-          }
-          foundResponse <-
-            responseOrError match {
-              case Right(found: HttpResponse.Found) => ZIO.succeed(found)
-              case Right(HttpResponse.NotFound)     => ZIO.succeed(HttpResponse.fromHttpCode.`404`)
-              case Left(error) =>
-                for {
-                  (specifiedResponseCode, errors) <- Handler.getHttpCodeAndErrors(error)
-                  responseCode =
-                    specifiedResponseCode match {
-                      case Some(responseCode) => responseCode
-                      case None =>
-                        if (errors.forall(_.isInstanceOf[HError.UserError])) HttpCode.`400`
-                        else HttpCode.`500`
-                    }
-                  _ <- ZIO.foreachDiscard(errors.toList) { e =>
-                    Logger.log.error(e.fullInternalMessage, "error-type" -> e.getClass.getName) *>
-                      Logger.log.debug(e.fullInternalMessageWithTrace, "error-type" -> e.getClass.getName)
+        responseOrError <- route(req.method, req.path).either
+        responseOrError <- responseOrError match {
+          case Left(earlyReturn: EarlyReturn) => Logger.log.debug("Received early return value").as(earlyReturn.response.asRight)
+          case other                          => ZIO.succeed(other)
+        }
+        foundResponse <-
+          responseOrError match {
+            case Right(found: HttpResponse.Found) => ZIO.succeed(found)
+            case Right(HttpResponse.NotFound)     => ZIO.succeed(HttpResponse.fromHttpCode.`404`)
+            case Left(error) =>
+              for {
+                (specifiedResponseCode, errors) <- Handler.getHttpCodeAndErrors(error)
+                responseCode =
+                  specifiedResponseCode match {
+                    case Some(responseCode) => responseCode
+                    case None =>
+                      if (errors.forall(_.isInstanceOf[HError.UserError])) HttpCode.`400`
+                      else HttpCode.`500`
                   }
-                } yield HttpResponse(errors.toList.map(_.userMessage.show(ifHidden)).toJson, responseCode)
-            }
+                _ <- ZIO.foreachDiscard(errors.toList) { e =>
+                  Logger.log.error(e.fullInternalMessage, "error-type" -> e.getClass.getName) *>
+                    Logger.log.debug(e.fullInternalMessageWithTrace, "error-type" -> e.getClass.getName)
+                }
+              } yield HttpResponse(errors.toList.map(_.userMessage.show(ifHidden)).toJson, responseCode)
+          }
 
-          _ <-
-            ZIO
-              .hAttempt {
-                val headers = exchange.getResponseHeaders
-                foundResponse.headers.foreach { (k, v) => headers.set(k, v) }
-                foundResponse.cookies.reverse.foreach { c => headers.add("Set-Cookie", c.cookieString) }
-              }
-              .mapError(HError.SystemFailure("Unable to write response cookies", _))
-          _ <- ZIO.hAttempt(exchange.sendResponseHeaders(foundResponse.code.code, foundResponse.length)).mapError(HError.SystemFailure("Unable to write response headers", _))
-          _ <- ZIO.hAttempt(foundResponse.write(responseBody)).mapError(HError.SystemFailure("Unable to write response body", _))
-        } yield ()).trace("HTTP Request Handler", Logger.LogLevel.Detailed, "path" -> req.pathString)
-      }
+        _ <-
+          ZIO
+            .hAttempt {
+              val headers = exchange.getResponseHeaders
+              foundResponse.headers.foreach { (k, v) => headers.set(k, v) }
+              foundResponse.cookies.reverse.foreach { c => headers.add("Set-Cookie", c.cookieString) }
+            }
+            .mapError(HError.SystemFailure("Unable to write response cookies", _))
+        _ <- ZIO.hAttempt(exchange.sendResponseHeaders(foundResponse.code.code, foundResponse.length)).mapError(HError.SystemFailure("Unable to write response headers", _))
+        _ <- ZIO.hAttempt(foundResponse.write(responseBody)).mapError(HError.SystemFailure("Unable to write response body", _))
+      } yield ()
 
     Unsafe.unsafe { implicit unsafe =>
       serverRuntime.unsafe.run {
@@ -79,7 +78,7 @@ final case class Handler[ServerEnv, ReqEnv: EnvironmentTag](
             .addContext("request-id" -> requestId) {
               effect
                 .provideSomeLayer[HarnessEnv & ServerEnv & Scope](builtInReqLayer ++ reqLayer)
-                .trace("Full HTTP Request Handler")
+                .trace("HTTP Request Handler", "path" -> exchange.getRequestURI.getPath)
             }
             .dumpErrorsAndContinue
         }
