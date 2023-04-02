9f7ae714f8984a2c40613f6c2cb427b0efd6c70e
Kalin-Rudnicki <kalin.r.rudnicki@gmail.com>
2023-04-02 13:45:08 -0600

    Updated template to return 401 for session-token

--- harness-web-app-template/api/src/main/scala/template/api/routes/Helpers.scala
+++ harness-web-app-template/api/src/main/scala/template/api/routes/Helpers.scala
@@ -2,6 +2,7 @@ package template.api.routes
 
 import harness.core.*
 import harness.sql.*
+import harness.web.*
 import harness.web.server.*
 import harness.zio.*
 import template.api.db.{model as M, queries as Q}
@@ -12,15 +13,21 @@ private[routes] object Helpers {
   // TODO (KR) : name this on a project specific basis
   val SessionToken: String = "Template-Session-Token"
 
-  val userFromSession: HRIO[JDBCConnection & Logger & HttpRequest, M.User.Identity] =
-    HttpRequest.cookie.get[String](Helpers.SessionToken).flatMap { tok =>
-      Q.User.fromSessionToken(tok).single
-    }
-
   val userFromSessionOptional: HRIO[JDBCConnection & Logger & HttpRequest, Option[M.User.Identity]] =
     HttpRequest.cookie.find[String](Helpers.SessionToken).flatMap {
-      case Some(tok) => Q.User.fromSessionToken(tok).single.asSome
-      case None      => ZIO.none
+      case Some(tok) =>
+        Q.User.fromSessionToken(tok).option.flatMap {
+          case Some(tok) => ZIO.some(tok)
+          case None =>
+            Logger.log.warning("Session token was specified, but is not valid") *>
+              HttpResponse.earlyReturn.fromHttpCode.json(HttpCode.`401`)
+        }
+      case None => ZIO.none
+    }
+
+  val userFromSession: HRIO[JDBCConnection & Logger & HttpRequest, M.User.Identity] =
+    userFromSessionOptional.someOrElseZIO {
+      ZIO.fail(HError.UserError(s"Unauthorized: Specify cookie '$SessionToken'").withHTTPCode(HttpCode.`401`))
     }
 
 }
